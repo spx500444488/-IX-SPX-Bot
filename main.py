32f0101f8f32335a8561d1613f4af66334c7f2a1
spx500444488 <i.b.r.55499@gmail.com>
2026-08-14 11:26:22 +0300

    Add asyncio import to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import os
+import asyncio
 from flask import Flask, request
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
